b7f59f1232709713df18cb77272ca3599f4e78d2
maryemmanuel001 <maryemmanuel379@gmail.com>
2017-07-21 14:01:51 +0100

    first commit

---
+++ coding3/formv.js
@@ -0,0 +1,35 @@
+<html>
+<head>
+<script type="text/javascript">
+function mary()
+{
+if (getElementById("e1").value=="")
+alert("enter your full name")
+else
+ if (getElementById("g3").value=="")
+alert("please kindly enter your email address")
+else
+if (getElementById("g4").value=="")
+alert("please kindly enter your password")
+else
+if (getElementById("g6").value=="")
+alert("please enter your phone number")
+else
+alert("Form submitted")
+}
+
+
+
+
+
+
+</script>
+</head>
+<body>
+
+
+
+
+
+</body>
+</html>
